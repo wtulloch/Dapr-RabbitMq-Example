95fea3b62289f3de58acfc7e2a85782514952682
wtulloch <bill.tulloch@gmail.com>
2024-03-15 20:02:13 +1100

    updated to preview 4 and switched to AddContainer

--- Dapr-RabbitMq-Example.AppHost/Program.cs
+++ Dapr-RabbitMq-Example.AppHost/Program.cs
@@ -5,28 +5,34 @@ using Aspire.Hosting.Dapr;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-//Uncomment the following to try with rabbitmq from Aspire
 
-//builder.AddRabbitMQContainer("messaging", password: "ThisIsAPassword")
-//    .WithEndpoint(hostPort: 5672, scheme: "http");
+builder.AddContainer("test-rabbit", "rabbitmq", "3-management")
+    .WithEnvironment("RABBITMQ_DEFAULT_PASS", "ThisIsAPassword")
+    .WithEnvironment("RABBITMQ_DEFAULT_USER", "user")
+    .WithEndpoint(scheme: "tcp", hostPort: 5672, containerPort: 5672,  isProxied: false)
+    .WithEndpoint(scheme: "http", hostPort: 15672, containerPort:15672, isProxied: false);
+
 
 
 builder.AddDapr();
 
-builder.AddProject<Projects.MessagePublisher>("MessagePublisher")
+builder.AddProject<Projects.MessagePublisher>("publisher")
     .WithDaprSidecar(new DaprSidecarOptions
     {
-        AppId = "message-publisher-service",
+        AppId = "message-publish-service",
         ResourcesPaths = ImmutableHashSet.Create("../components/"),
         LogLevel = "warn"
     });
+   
 
-builder.AddProject<Projects.MessageReceiver>("MessageReceiver")
+builder.AddProject<Projects.MessageReceiver>("subscriber")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "message-receiver-service",
         ResourcesPaths = ImmutableHashSet.Create("../components/"),
         LogLevel = "warn"
     });
+
+
 
 builder.Build().Run();
